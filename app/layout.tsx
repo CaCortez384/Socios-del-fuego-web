3bed9dc3ab1663b097e9962139b56a34cbffb9f8
CaCortez384 <ca.cortez384@gmail.com>
2026-02-19 18:38:19 -0300

    fix: actualizar ID de Facebook Pixel y mejorar la implementación del script

--- app/layout.tsx
+++ app/layout.tsx
@@ -140,32 +140,35 @@ export default function RootLayout({
             gtag('config', 'G-B5PB45XGP9');
           `}
         </Script>
-        {/* --- META PIXEL (FACEBOOK) --- */}
-        <Script id="fb-pixel" strategy="afterInteractive">
+
+
+        {/* --- META PIXEL (NUEVO ID) --- */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
           {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
+    !function(f,b,e,v,n,t,s){
+      if(f.fbq) return;
+      n = f.fbq = function(){
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if(!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e); t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s);
+    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '1290748873104371');
     fbq('track', 'PageView');
   `}
         </Script>
-
-        {/* Respaldo para navegadores sin JavaScript */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1290748873104371&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1290748873104371&ev=PageView&noscript=1" />'
+          }}
+        />
 
         {/* --- MICROSOFT CLARITY --- */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
