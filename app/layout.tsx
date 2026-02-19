05bbae9673b600f27dda69125d0aa7b697514109
CaCortez384 <ca.cortez384@gmail.com>
2026-02-19 18:27:56 -0300

    cambio pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -151,7 +151,7 @@ export default function RootLayout({
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1414525117081008');
+    fbq('init', '1290748873104371');
     fbq('track', 'PageView');
   `}
         </Script>
@@ -162,7 +162,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1414525117081008&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1290748873104371&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
